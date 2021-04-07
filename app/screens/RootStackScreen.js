ded723a2438fdd8019bd988973a8d94877ccf92b
Shivansh Pratap <46789950+acousticclown@users.noreply.github.com>
2021-04-07 15:06:03 +0530

    Fixed Login Screen Duplicasy 🚑

--- app/screens/RootStackScreen.js
+++ app/screens/RootStackScreen.js
@@ -4,7 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import SplashScreen from "./SplashScreen";
 import SignupScreen from "./SignupScreen";
-import LoginPage from "./LoginPage";
+import LoginScreen from "./LoginScreen";
 
 const RootStack = createStackNavigator();
 
@@ -12,7 +12,7 @@ const RootStackScreen = ({ navigation }) => (
   <RootStack.Navigator headerMode="none">
     <RootStack.Screen name="Splash" component={SplashScreen} />
     <RootStack.Screen name="Signup" component={SignupScreen} />
-    <RootStack.Screen name="Login" component={LoginPage} />
+    <RootStack.Screen name="Login" component={LoginScreen} />
   </RootStack.Navigator>
 );
 
