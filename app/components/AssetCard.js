9b835168a0c1065e9ffdc183b0a8f7c90b782663
Shivansh Pratap <46789950+acousticclown@users.noreply.github.com>
2021-05-26 21:37:05 +0530

    AssetCard Updated 🔧

--- app/components/AssetCard.js
+++ app/components/AssetCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { TouchableOpacity } from "react-native";
 import { StyleSheet, Text, View } from "react-native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
@@ -12,9 +13,9 @@ const AssetCard = ({
   iconName,
 }) => {
   return (
-    <View style={styles.container}>
+    <TouchableOpacity style={styles.container}>
       <View style={styles.icon}>
-        <MaterialCommunityIcons name={icon} color="#7F5DF0" size={30} />
+        <MaterialCommunityIcons name={icon} color="#7F5DF0" size={40} />
       </View>
       <View style={styles.info}>
         <Text style={styles.name}>{name}</Text>
@@ -23,15 +24,15 @@ const AssetCard = ({
       <View style={styles.value}>
         <Text style={styles.worth}>{worth}</Text>
         <View style={styles.arrow}>
-          <MaterialCommunityIcons
+          {/* <MaterialCommunityIcons
             name={iconName}
             color={changeColor}
             size={30}
-          />
+          /> */}
           <Text style={(styles.change, { color: changeColor })}>{change}</Text>
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
@@ -40,16 +41,18 @@ export default AssetCard;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: "100%",
+    width: "90%",
     height: 80,
     backgroundColor: "#FFF",
     marginVertical: 10,
     borderRadius: 15,
-    padding: 20,
+    padding: 18,
     flexDirection: "row",
+    elevation: 8,
+    alignSelf: "center",
   },
   icon: {
-    marginRight: 30,
+    marginRight: 15,
     alignItems: "center",
     justifyContent: "center",
   },
